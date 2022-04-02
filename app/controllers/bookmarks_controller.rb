6cb65fe1c3c90fe549fa5e8d8c37efa09cc1a08b
Mahtab Mahtdavifar <mhtbmahdavifar@gmail.com>
2022-04-02 14:59:17 +0200

    color and fornt SCSS files added

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -6,8 +6,9 @@ class BookmarksController < ApplicationController
 
   def create
     @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
     @bookmark.list = @list
-      if bookmark.save
+      if @bookmark.save
         redirect_to list_path(@list)
       else
         render :new
@@ -23,7 +24,6 @@ class BookmarksController < ApplicationController
   private
 
   def bookmark_params
-    params.require(:bookmark).permit(:comment)
+    params.require(:bookmark).permit(:comment, :movie_id)
   end
-
 end
