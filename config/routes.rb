8b703ecb95969bba0139bf19af94f34f45a4fffe
Mahtab Mahtdavifar <mhtbmahdavifar@gmail.com>
2022-04-21 09:23:16 +0200

    update fonts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 root to: 'lists#index'
+get 'ui_kit', to: 'pages#ui_kit'
 resources :lists, except: [:edit, :update] do
   resources :bookmarks, only: [:new, :create]
 end
