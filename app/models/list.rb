9ad25b26573fc3cef1d034e4367532f62344bb86
Mahtab Mahtdavifar <mhtbmahdavifar@gmail.com>
2022-04-22 13:38:53 +0200

    Add dotenv - Protect my secret data in .env file

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,4 +2,5 @@ class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   validates :name, uniqueness: true
   has_many :movies, through: :bookmarks
+  has_one_attached :image
 end
