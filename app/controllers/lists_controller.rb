5de34a771d2cd20d34624f7f056ffce7a1d84823
Mahtab Mahtdavifar <mhtbmahdavifar@gmail.com>
2022-04-25 09:46:16 +0200

    connect logo with homepage

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,34 +1,36 @@
 class ListsController < ApplicationController
   def index
-    @lists = List.all
-  end
+		@lists = List.all
+	end
 
-  def new
-    @list = List.new
-  end
+	def show
+		@list = List.find(params[:id])
+	end
 
-  def create
-    @list = List.new(list_params)
-      if @list.save
-      redirect_to list_path(@list)
-    else
-      render :new
-      end
-  end
+# instantiate the form
+	def new
+		@list = List.new
+	end
 
-  def show
-    @list = List.find(params[:id])
-  end
+	def create
+		@list = List.new(list_params)
+		if @list.save
+			redirect_to list_path(@list)
+		else
+			# when writes but doesnt save, nothing happens
+			render :new
+		end
+	end
 
-  def destroy
-    @list = List.find(params[:id])
-    @list.destroy
-    redirect_to lists_path
-  end
+	def destroy
+		@list = List.find(params[:id])
+		@list.destroy
+		redirect_to lists_path
+	end
 
-  private
-
-  def list_params
-    params.require(:list).permit(:name, :image)
-  end
+	private
+#whitelisting for security
+	def list_params
+		params.require(:list).permit(:name, :image)
+	end
 end
